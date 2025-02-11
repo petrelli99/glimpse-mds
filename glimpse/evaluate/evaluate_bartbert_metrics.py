fa163b982c2bc906ab1d60a252920d4db751bb6f
matteo-petrelli <mpetrelli99@gmail.com>
2025-02-11 19:18:18 +0100

    Update evaluate_bartbert_metrics.py

--- glimpse/evaluate/evaluate_bartbert_metrics.py
+++ glimpse/evaluate/evaluate_bartbert_metrics.py
@@ -88,7 +88,7 @@ def main():
     # check if exists already, if it does load it and add the new columns
 
     print(df)
-
+    '''
     if path.exists():
         df_old = pd.read_csv(path, index_col=0)
 
@@ -104,6 +104,39 @@ def main():
         df = df_old
 
     df.to_csv(path)
+    '''
+
+    '''
+    if path.exists():
+    # Leggi il vecchio file CSV
+        df_old = pd.read_csv(path)
+    
+    # Aggiungi la nuova colonna solo se non esiste
+        for col in df.columns:
+            if col not in df_old.columns:
+                df_old[col] = None
+    
+    # Aggiungi nuove righe al DataFrame
+        df = pd.concat([df_old, df], ignore_index=True)
+
+    df.to_csv(path, index=False)
+    '''
+    if path.exists():
+        df_old = pd.read_csv(path)
+    
+    # Verifica e aggiungi eventuali colonne mancanti
+        for col in df.columns:
+            if col not in df_old.columns:
+                df_old[col] = None
+
+    # Sostituisci i valori solo se necessario
+        for col in df.columns:
+            df_old[col].update(df[col])
+
+        df = df_old
+
+    df.to_csv(path, index=False)
+
 
 
 if __name__ == "__main__":
